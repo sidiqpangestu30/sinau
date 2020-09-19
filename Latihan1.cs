e79599beefda05b0ca40659449b3ef3bd6ff92ed
sidiqpangestu30 <sidiqpangestu30@gmail.com>
2020-09-19 20:09:10 +0700

    Add files via upload

---
+++ Latihan1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Saya Sidiq Pangestu adalah Game Developer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
